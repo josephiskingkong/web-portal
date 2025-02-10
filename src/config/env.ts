fc2664ffb903f32f700e00afbc62670a28b85dbb
Egor <117137864+josephiskingkong@users.noreply.github.com>
2025-02-10 12:53:17 +0700

    dev: change some logs and errors texts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const ENV = {
 	NODE_ENV: process.env.NODE_ENV || 'dev',
-	PORT: process.env.PORT || 5000,
+	PORT: process.env.PORT || 5050,
 	JWT_SECRET: process.env.JWT_SECRET || 'default_secret',
 	MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo:27017/mydatabase',
 };
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,12 +5,12 @@ import bcrypt from 'bcrypt';
 import { ENV } from '../config/env';
 
 export const register = async (req: Request, res: Response) => {
-	try {
-		const user = await UserService.registerUser(req.body);
-		res.status(201).json({ message: 'User created', user });
-	} catch (error: any) {
-		res.status(400).json({ error: error.message });
-	}
+  try {
+    const user = await UserService.registerUser(req.body);
+    res.status(201).json({ message: 'User created', user });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const login = async (req: Request, res: Response): Promise<void> => {
@@ -19,13 +19,13 @@ export const login = async (req: Request, res: Response): Promise<void> => {
 
     const user = await UserService.findUserByLogin(login);
     if (!user) {
-      res.status(404).json({ error: "Пользователь не найден" });
+      res.status(404).json({ error: "User not found" });
       return;
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      res.status(401).json({ error: "Неверный пароль" });
+      res.status(401).json({ error: "Incorrect password" });
       return;
     }
 
@@ -35,10 +35,10 @@ export const login = async (req: Request, res: Response): Promise<void> => {
       { expiresIn: "7d" }
     );
 
-    const userWithoutPassword = {...user.toObject(), password: undefined};
+    const userWithoutPassword = { ...user.toObject(), password: undefined };
     res.status(200).json({ token, user: userWithoutPassword });
   } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : "Неизвестная ошибка";
+    const errorMessage = error instanceof Error ? error.message : "Unkown error";
     res.status(500).json({ error: errorMessage });
   }
 };
