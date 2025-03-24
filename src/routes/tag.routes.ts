835ce5b52059c54bd793efca3b3b46d71eda9697
Egor <117137864+josephiskingkong@users.noreply.github.com>
2025-03-24 13:05:05 +0700

    fix: tag swagger

--- src/routes/tag.routes.ts
+++ src/routes/tag.routes.ts
@@ -5,31 +5,10 @@ const router = Router();
 
 /**
  * @swagger
- * components:
- *   schemas:
- *     Tag:
- *       type: object
- *       properties:
- *         _id:
- *           type: string
- *           example: "60d21baee97f8e2f30e3e3ef"
- *         title:
- *           type: string
- *           example: "JavaScript"
- *         slug:
- *           type: string
- *           example: "javascript"
- *         description:
- *           type: string
- *           example: "Programming language for web development."
- *         createdAt:
- *           type: string
- *           format: date-time
- *           example: "2023-05-30T14:12:30Z"
- *
- * /tags:
+ * /api/tags/:
  *   post:
  *     summary: Create a new tag
+ *     tags: [Tags]
  *     description: Create a new tag.
  *     requestBody:
  *       required: true
